7edcb3681d86c81af02a8c06850a14d2511e5d13
Neville Oronni <nevooronni@gmail.com>
2018-01-06 02:06:06 +0300

    modified last column for portfolio and home page and added github button and links

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 //custom jquery functions
 
 $(document).ready(function() {
+	$("#iconsocial").slideDown(2200);
+	$("#iconsocial2").slideDown(2800);
 	$(".white, .red").fadeIn(1000);
 	$(".black").fadeIn(2000)
 	$("#sidebar").slideDown(500);
+	$("#devdev").slideDown(1100);
+	$("#description3").slideDown(2200);
 	$("#description").slideDown(1100);
 	$("#description2").slideDown(1400);
 	$("#devstack").slideDown(2200);
 	$("#fullstack").fadeIn(2000);
 	$("#fullstack").animate({backgroundColor: '#fff'}, 'slow');
-	$("#iconsocial").slideDown(2200);
+	$("#fullstack2").fadeIn(2000);
+	$("#fullstack2").animate({backgroundColor: '#fff'}, 'slow');
 });
