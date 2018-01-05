113fca34614f8656039abd1f239868d0db317bd6
Neville Oronni <nevooronni@gmail.com>
2018-01-05 03:17:43 +0300

    added portfolio page

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@ $(document).ready(function() {
 	$(".black").fadeIn(2000)
 	$("#sidebar").slideDown(500);
 	$("#description").slideDown(1100);
+	$("#description2").slideDown(1400);
 	$("#devstack").slideDown(2200);
 	$("#fullstack").fadeIn(2000);
-	$('#fullstack').animate({backgroundColor: '#fff'}, 'slow');
+	$("#fullstack").animate({backgroundColor: '#fff'}, 'slow');
+	$("#iconsocial").slideDown(2200);
 });
