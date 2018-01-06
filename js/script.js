acb9c8bd6ec3e30a0447292421b85f3e9a19421c
Neville Oronni <nevooronni@gmail.com>
2018-01-06 20:46:46 +0300

    added contact page

--- js/script.js
+++ js/script.js
@@ -6,12 +6,16 @@ $(document).ready(function() {
 	$(".white, .red").fadeIn(1000);
 	$(".black").fadeIn(2000)
 	$("#main").fadeIn(2000);
+	$(".contactme").fadeIn(2000);
+	$(".white2").fadeIn(2500);
+	$(".red2").fadeIn(3000);
 	$(".languages").fadeIn(3000);
 	$("#sidebar").slideDown(500);
 	$("#devdev").slideDown(1100);
 	$("#description3").slideDown(2200);
 	$("#description").slideDown(1100);
 	$("#description2").slideDown(1400);
+	$("#description4").slideDown(1400);
 	$("#devstack").slideDown(2200);
 	$("#fullstack").fadeIn(2000);
 	$("#fullstack").animate({backgroundColor: '#fff'}, 'slow');
